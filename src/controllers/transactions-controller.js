f5e90be041b9a783b50e2d2692241d446f2e831d
truds99 <dudscardoso9@gmail.com>
2024-07-18 23:42:25 -0300

    add get transactions

--- src/controllers/transactions-controller.js
+++ src/controllers/transactions-controller.js
@@ -1,11 +1,29 @@
 import httpStatus from "http-status";
 import { db } from "../config/database.js";
+import { ObjectId } from "mongodb";
 
 export async function postTransactions(req, res) {
     const transaction = {...req.body, userId: res.locals.user._id};
     try { 
         await db.collection("transactions").insertOne(transaction)
         res.sendStatus(httpStatus.CREATED);
+    }
+    catch (err){
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err.message);
+    } 
+}
+
+export async function getTransactions(req, res) {
+    const page = req.query.page || 1;
+    const items = 10;
+    const start = (page - 1) * items;
+    const userId = res.locals.user._id;
+
+    if (page % 1 !== 0 || page < 1) return res.sendStatus(httpStatus.BAD_REQUEST);
+    try { 
+        let transactions = await db.collection("transactions").find( { userId: new ObjectId(userId) }).toArray();
+        transactions = transactions.reverse().slice(start, start + items);
+        res.send(transactions).status(httpStatus.OK);
     }
     catch (err){
         res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err.message);
--- src/routers/transactions-router.js
+++ src/routers/transactions-router.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import { postTransactions } from "../controllers/transactions-controller.js";
+import { postTransactions, getTransactions } from "../controllers/transactions-controller.js";
 import { validateToken } from "../middlewares/auth-middleware.js";
 import { validateSchema } from "../middlewares/schemas-middleware.js";
-import { transactionSchema } from "../schemas/transactions-schemas.js"
+import { transactionSchema } from "../schemas/transactions-schemas.js";
 
 const transactionsRouter = Router();
 
-transactionsRouter.use(validateToken)
+transactionsRouter.use(validateToken);
 transactionsRouter.post('/transactions', validateSchema(transactionSchema), postTransactions);
+transactionsRouter.get('/transactions', getTransactions);
 
 export default transactionsRouter;
